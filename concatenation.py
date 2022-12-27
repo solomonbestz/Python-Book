63e44c11a3b6415b586691f271b756acfb843da2
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-27 19:05:37 +0100

    modified

---
+++ concatenation.py
@@ -0,0 +1,31 @@
+# CONCATENATION
+
+"""
+Concatenation takes you to another level of coding, with this you can join
+two or strings together, in cases where your variable values can change anytime concatenation
+makes it easier, shorter and better. There are different ways of concatenations but i would show you
+the basic one which is the + character, and later on a more advance one which is the f-string(string manipulation)
+"""
+first_name = "Solomon"
+last_name = "Bestz"
+
+"""
+We would add an empty string with space in between our first_name and last_name,
+so the output would look more nicer
+"""
+# print(first_name+" "+last_name) 
+
+
+# PROGRAM TO SUM TWO NUMBERS
+
+"""
+We would be summing two numbers with this program
+"""
+
+first_num = int(input("Enter First Number: "))
+second_num = int(input("Enter Second Number: "))
+
+
+sum_num = first_num + second_num
+
+print(f"The answer is: {sum_num}")
---
+++ print-method.py
@@ -0,0 +1,49 @@
+# HELLO WORLD
+
+"""
+We would be writing out to the terminal saying hello world
+"""
+
+# print("Hello World!")
+
+#NEW LINE 
+
+"""
+We would use the ability of the print statement to write to multiple sentences in our print method
+"""
+
+# print("My name is Solomon Bestz \nI am a Computer Programmer \nProgramming is fun.")
+
+
+# INPUT METHOD
+"""
+The input method allows us to write in data into our program
+
+and Our Bag Store Program would be modified using the input method
+"""
+
+"""
+Whaterver the user writes as his or her name, the data would be stored in name variable
+we could print out the users data using our print method, remember the print method converts all data to strins
+before printing to the terminal
+"""
+# name = input("What is your name? ") 
+# print(name)
+
+
+
+
+# LET US UPDATE OUR BAG STORE PROGRAM
+
+first_bag_name = "Gucci" # A string variable to hold the name of our bag product
+
+is_in_stock_first_bag = True # Checking if our bag is in stock
+
+price_tag_first_bag = 23000.00 # A float variable to hold our price tag
+
+quantity_first_bag = 20 # We have 20 Gucci bags in store
+
+second_bag_name = input("Enter A New Bag: ")
+
+print(first_bag_name)
+print(second_bag_name)
