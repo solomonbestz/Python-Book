dddc026ae8ab1a2d9e02210cd89ef6a69b0f9684
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-28 19:12:32 +0100

    modified

--- keywords.py
+++ keywords.py
@@ -29,13 +29,29 @@ This keywords are used to start a conditional statement, they are 'if',
 
 color = "red"
 
-if color == "black":
-    print("Black")
-elif color == "green":
-    print("Green")
-elif color == "red":
-    print("red")
-else:
-    print("Color Not Found.")
+# if color == "black":
+#     print("Black")
+# elif color == "green":
+#     print("Green")
+# elif color == "red":
+#     print("red")
+# else:
+#     print("Color Not Found.")
 
+
+# Iteration Keywords
+
+"""
+The Iteration keywords are used in looping through collection of items, let's take a look
+at for, while, break, continue, else
+"""
+
+my_list = [ 1, 2, 3, 4, 5, 6]
+count = 1
+
+while len(my_list) > 7:
+    print(count)
+    count += 1
+else:
+    print("Our While condition wasn't met.")
 
