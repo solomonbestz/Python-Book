4b6a868eb2603539cfa0e5d77e27a8b8dc17b707
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-30 14:59:52 +0100

    modified

---
+++ loops.py
@@ -0,0 +1,39 @@
+# LOOPS
+
+"""
+INTRO TO LIST
+"""
+"""
+In our street variable we have three houses, 
+say I boarded a bike going to house3 i wont be 
+able to get to house3 if i don't know the address and in our case our
+house numbering starts from index 0
+"""
+"""
+Getting the house isn't difficult, we just need to 
+call the variable and place in the number in a square bracket 
+and we can access the house that way..
+"""
+
+# street = ['house1', 'house2', 'house3']
+
+# for house in street:
+#     print(house)
+
+"""
+The above loop we have written would print out all the houses in our
+street list
+"""
+
+
+numbers = [4, 6, 7, 8, 9, 12, 71, 2, 10, 1]
+sum_number = 0
+
+for number in numbers:
+    sum_number = sum_number + number
+
+print(sum_number)
+
+
+    
+
