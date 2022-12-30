239e42266febd643075546a8fde4c58e9fdcb98b
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-30 18:15:50 +0100

    modified

--- loops.py
+++ loops.py
@@ -32,8 +32,40 @@ sum_number = 0
 for number in numbers:
     sum_number = sum_number + number
 
-print(sum_number)
+# print(sum_number)
 
+# WHILE LOOP 
 
+initial = 0
+
+end_loop = 10
+
+while initial < end_loop:
+    # print(initial)
+    initial = initial + 1
     
 
+#GUESS GAME
+
+secret_number = 8
+start_game = 0
+stop_game = 5
+
+print("Guess My Secret Number")
+
+while start_game < stop_game:
+    user_input = int(input("Enter Number: "))
+
+    if user_input == secret_number:
+        print("You Won!!! Genius")
+        break
+    else:
+        if (stop_game-1)-start_game != 0 and (stop_game-1)-start_game != 1:
+            print(f"Try Again, {(stop_game-1)-start_game} guesses left")
+        elif (stop_game-1)-start_game == 1:
+            print(f"Try Again, {(stop_game-1)-start_game} guess left")
+        else:
+            print(f"{(stop_game-1)-start_game} guess left")
+    start_game = start_game + 1
+else:
+    print("Game Over!!! You Lost")
