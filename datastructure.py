e51d300242b769a906069357e19e8c2a56d7a8b9
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-31 21:36:48 +0100

    modified

--- datastructure.py
+++ datastructure.py
@@ -20,3 +20,33 @@ numbers = (2, 4, 3, 1)
 set_of_names = set()
 
 # print(type(set_of_names))
+
+# DICTIONARY DATA STRUCTURE
+
+students = {"Seat-1": "Solomon", "Seat-2": "David", "Seat-3": "Daniel"}
+
+print(students["Seat-2"])
+
+
+
+
+
+
+
+# import time 
+
+# start_time = time.time()
+# my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+# my_dict = {1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"10",
+#             11:"11",12:"12",13:"13",14:"14",15:"15",16:"16",17:"17",18:"18",19:"19",20:"20"}
+
+# for n in my_list:
+#     if 18 == n:
+#         print("Found")
+
+# print(my_dict[18])
+
+# end_time = time.time()
+
+# print(f"Time spent to get 18 is: {end_time-start_time}")
