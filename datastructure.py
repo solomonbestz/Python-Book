6af688f1d82120f9d6e61484fb3199579036da76
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-31 18:19:46 +0100

    modified

---
+++ datastructure.py
@@ -0,0 +1,22 @@
+# DATA STRUCTURE
+
+# LIST DATA STRUCTURE
+
+lis = [3, 'a', 4.5, 3, "Solomon"]
+
+lis.remove(3)
+
+# print(lis)
+
+
+# TUPLE DATA STRUCTURE
+
+numbers = (2, 4, 3, 1)
+
+# print(numbers[2])
+
+# SET DATA STRUCTURE
+
+set_of_names = set()
+
+# print(type(set_of_names))
