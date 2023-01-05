c062dbfbd0dfde050024b24bb1be54aeb627dce5
Solomon Bestz <Kingsolodawarrior@gmail.com>
2023-01-05 21:35:24 +0100

    modified

--- code sample beginners/print-method.py
+++ code sample beginners/print-method.py
@@ -21,29 +21,34 @@ The input method allows us to write in data into our program
 
 and Our Bag Store Program would be modified using the input method
 """
+#input() #This input method allows us write in data to our program
+
 
 """
-Whaterver the user writes as his or her name, the data would be stored in name variable
-we could print out the users data using our print method, remember the print method converts all data to strins
+Whaterver the user writes as his or her name, the data would be stored in name 
+variable
+we could print out the users data using our print method, remember the print method
+converts all data to strins
 before printing to the terminal
 """
 # name = input("What is your name? ") 
 # print(name)
 
+# print("What is your name? ")
+# name = input()
+# print(name)
 
 
 
 # LET US UPDATE OUR BAG STORE PROGRAM
 
 first_bag_name = "Gucci" # A string variable to hold the name of our bag product
-
 is_in_stock_first_bag = True # Checking if our bag is in stock
-
 price_tag_first_bag = 23000.00 # A float variable to hold our price tag
-
 quantity_first_bag = 20 # We have 20 Gucci bags in store
-
 second_bag_name = input("Enter A New Bag: ")
 
 print(first_bag_name)
 print(second_bag_name)
+
+
