3ac2830dae6ed3d87c75ed5c0b31a3744f18d626
Solomon Bestz <Kingsolodawarrior@gmail.com>
2023-01-06 09:51:53 +0100

    modified

--- code sample beginners/conditional.py
+++ code sample beginners/conditional.py
@@ -24,19 +24,46 @@ operator = '-'
 #     print(f"{operator} is an invalid operator")
 
 
+# if operator == '+':
+#     answer = num_1 + num_2
+#     print(f'Answer: {answer}')
+# else:
+#     print(f"{operator} doesn't have a True if block.")
+
+# if operator == '+':
+#     answer = num_1 + num_2
+#     print(f'Answer: {answer}')
+# elif operator == '-':
+#     answer = num_1 - num_2
+#     print(f"Answer: {answer}")
+# elif operator == '*':
+#     answer = num_1 * num_2
+#     print(f"Answer {answer}")
+# elif operator == "/":
+#     answer = num_1 / num_2
+#     print(f"Answer {answer}")
+# else:
+#     print(f"{operator} is an invalid operator")
+
+
 # Using and/ or in checking condtions
 
-n = int(input("Enter Number: "))
+# n = int(input("Enter Number: "))
 
-if n % 2 == 1:
-    print("Weird")
-elif (n % 2 == 0) and (n == 2 or n <= 5):
-    print("Not Weird")
-elif (n % 2 == 0) and (n == 6 or n <= 20):
-    print("Weird")
-elif n % 2 == 0 and n > 20:
-    print("I said Not Weird")
+# if n % 2 == 1:
+#     print("Weird")
+# elif (n % 2 == 0) and (n == 2 or n <= 5):
+#     print("Not Weird")
+# elif (n % 2 == 0) and (n == 6 or n <= 20):
+#     print("Weird")
+# elif n % 2 == 0 and n > 20:
+#     print("I said Not Weird")
+# n = 4
 
+# if n % 2 == 1:
+#     print("Weird")
+# else:
+#     print("Not Weird")
 
 
 
@@ -55,12 +82,12 @@ if "S" in "Solomon":
     # print("Yes")
     pass
 
-
 def my_name():
     # print("Welcome Solomon Bestz")
     pass
-
 """
 The two examples above are blocks of code and a colon is placed at the front of an expression then
 the indentation follows the next line. Without the colon the block is void.
 """
+
+
