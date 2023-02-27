f06a6312a4924edf77510f3fe10054213baefb7b
Solomon Bestz <Kingsolodawarrior@gmail.com>
2023-02-27 09:39:01 +0100

    add code exercise to the beginners exercise folder

---
+++ Exercise beginners/module_countdown.py
@@ -0,0 +1,15 @@
+import time
+import os
+
+countdown = 20
+
+while countdown > 0:
+    countdown = countdown - 1
+    print(f"""
+    
+    
+                                                             {countdown}
+    
+    """)
+    time.sleep(1)
+    os.system('cls')
---
+++ Exercise beginners/module_madlibs.py
@@ -0,0 +1,17 @@
+mad_libs = """
+
+It was ___(FOOD)___ day at school, and ___(NAME)___ was super ___(ADJECTIVE)___ for lunch. 
+But when she went outside to eat, a ___(NOUN)___ stole her ___(FOOD)___! ___(NAME)___ chased the ___(NOUN)___ all over school. 
+She ___(VERB1)___, ___(VERB2)___, and ___(VERB3)___ through the playground. 
+Then she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped! Luckily, 
+___(NAME)___’s friends were willing to share their ___(FOOD)___ with her.
+
+"""
+
+answers = ["pizza", "solomon", "excited", "bird", "pizza", "solomon", "bird", "climbed", "jumped", "ran", "shoelace", "bird", "solomon", "pizza"]
+
+
+print("This is a Pizza Mad Lib Program Fill in All the brackets with the appropriate word")
+
+while True:
+    pass
